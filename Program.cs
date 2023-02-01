5a72ceb828c9e1e8d521b458973bf1dc1bbefe68
JoelAmores95 <joel.amores95@gmail.com>
2023-02-01 16:53:31 +0100

    Leer y escribir

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Escribir Texto
+            string texto = "Hola mundo";
+            string ok = "Funciona bien";
+            
+            // Crea o Sobreescribe el archivo
+            File.WriteAllText("miArchivo.txt", texto + "\n" + ok); 
+
+            // Leer Texto
+            string textoLeido = File.ReadAllText("miArchivo.txt");
+            Console.WriteLine(textoLeido);
+        }
+    }
+}
